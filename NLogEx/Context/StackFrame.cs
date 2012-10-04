f6c91ecfa5288fa2cd2fcd16c947e74127cc4801
bspell1 <bspell1@gmail.com>
2012-10-04 16:55:12 -0500

    Added the StackFrame ambient context

---
+++ NLogEx/Context/StackFrame.cs
@@ -0,0 +1,63 @@
+﻿//===========================================================================
+// MODULE:  StackFrame.cs
+// PURPOSE: runtime stack frame logging context
+// 
+// Copyright © 2012
+// Brent M. Spell. All rights reserved.
+//
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU Lesser General Public License as published 
+// by the Free Software Foundation; either version 3 of the License, or 
+// (at your option) any later version. This library is distributed in the 
+// hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU Lesser General Public License for more details. You should 
+// have received a copy of the GNU Lesser General Public License along with 
+// this library; if not, write to 
+//    Free Software Foundation, Inc. 
+//    51 Franklin Street, Fifth Floor 
+//    Boston, MA 02110-1301 USA
+//===========================================================================
+// System References
+using System;
+using System.Collections.Generic;
+using System.Linq;
+// Project References
+
+namespace NLogEx.Context
+{
+   /// <summary>
+   /// The stack frame logging context
+   /// </summary>
+   /// <remarks>
+   /// This class provides support for retrieving
+   /// the properties of the runtime stack at the
+   /// point at which an event was logged.
+   /// </remarks>
+   public static class StackFrame
+   {
+      /// <summary>
+      /// The current CLR stack frame object
+      /// </summary>
+      public static System.Diagnostics.StackFrame Frame
+      {
+         get { return new System.Diagnostics.StackFrame(7, true); }
+      }
+      /// <summary>
+      /// Establishes the stack frame context with the logging subsystem
+      /// </summary>
+      public static void Register ()
+      {
+         Log.RegisterContext(
+            "StackFrame",
+            new Dictionary<String, Func<Object>>()
+            {
+               { "File", () => Frame.GetFileName() },
+               { "Line", () => Frame.GetFileLineNumber() },
+               { "Type", () => Frame.GetMethod().DeclaringType.FullName },
+               { "Method", () => Frame.GetMethod().Name },
+            }
+         );
+      }
+   }
+}
---
+++ Test/NLogEx.Test/Context/TestStackFrame.cs
@@ -0,0 +1,74 @@
+﻿//===========================================================================
+// MODULE:  TestStackFrame.cs
+// PURPOSE: stack frame logging context unit test driver
+// 
+// Copyright © 2012
+// Brent M. Spell. All rights reserved.
+//
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU Lesser General Public License as published 
+// by the Free Software Foundation; either version 3 of the License, or 
+// (at your option) any later version. This library is distributed in the 
+// hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+// implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// See the GNU Lesser General Public License for more details. You should 
+// have received a copy of the GNU Lesser General Public License along with 
+// this library; if not, write to 
+//    Free Software Foundation, Inc. 
+//    51 Franklin Street, Fifth Floor 
+//    Boston, MA 02110-1301 USA
+//===========================================================================
+// System References
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// Project References
+
+namespace NLogEx
+{
+   #if !DEBUG     // stack frame is only reliable in debug mode
+      [Ignore]
+   #endif
+   [TestClass]
+   public class TestStackFrame
+   {
+      static TestStackFrame ()
+      {
+         Context.StackFrame.Register();
+      }
+
+      [TestMethod]
+      public void TestContext ()
+      {
+         var queue = new Loggers.Queue();
+         var log = new Log(this);
+         using (Log.RegisterInstance(
+               new Instance()
+               {
+                  Properties = new[]
+                  {
+                     "StackFrame.File",
+                     "StackFrame.Line",
+                     "StackFrame.Type",
+                     "StackFrame.Method"
+                  },
+                  Logger = queue,
+                  Buffer = 0,
+                  Synchronous = true
+               }
+            )
+         )
+         {
+            var frame = new System.Diagnostics.StackFrame(0, true);
+            log.Info("test");
+            var evt = queue.Dequeue().Single();
+            Assert.AreEqual(evt["StackFrame.File"], frame.GetFileName());
+            Assert.AreEqual(evt["StackFrame.Line"], frame.GetFileLineNumber() + 1);
+            Assert.AreEqual(evt["StackFrame.Type"], frame.GetMethod().DeclaringType.FullName);
+            Assert.AreEqual(evt["StackFrame.Method"], frame.GetMethod().Name);
+         }
+      }
+   }
+}
